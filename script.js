2587d16654aaab3f36dd71b44e0551737d2bbaf4
Karuna Sato <karutimes2@comcast.net>
2020-07-12 22:46:04 -0700

    added quiz questions to JS

---
+++ script.js
@@ -0,0 +1,44 @@
+let questions = [
+    {
+        question: "The condition in an if/else statement is enclosed within _____.",
+        A: "quotes",
+        B: "curly brackets",
+        C: "parentheses",
+        D: "square brackets",
+        answer: "B"
+
+    },
+    {
+        question: "Arrays in JavaScript can be used to store_____.",
+        A: "numbers and strings",
+        B: "other arrays",
+        C: "booleans",
+        D: "all of the above",
+        answer: "D"
+    },
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        A: "commas",
+        B: "curly brackets",
+        C: "quotes",
+        D: "parentheses",
+        answer: "C"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        A: "JavaScript",
+        B: "terminal/bash",
+        C: "for loops",
+        D: "console.log",
+        answer: "D"
+    },
+    {
+        question: "Commonly used data types DO NOT include _____.",
+        A: "strings",
+        B: "blooleans",
+        C: "alerts",
+        D: "numbers",
+        answer: "C"
+
+    }
+];
